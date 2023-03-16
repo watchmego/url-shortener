821e7119bb6ad38470c297901f5e039a9d8d1a7a
Reuben White <white.reuben@gmail.com>
2023-03-16 14:37:35 +1300

    test

--- backend/index.js
+++ backend/index.js
@@ -1,49 +1,84 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 const app = express();
 const port = 8000;
-import { generateString } from './utils/urlBuilder.js';
-import { insertOne, findOne, findOneAndUpdate } from './utils/db/queries.js';
+import { generateString } from "./utils/urlBuilder.js";
+import { insertURL, findURL, incrementURLVisitCount } from "./utils/db/queries.js";
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/url/:id', async (req, res) => {
-    const query = await findOne("shortURL", req.params.id);
-    console.log(query);
-    if(query) {
-        const result = {id: query.shortURL, originalURL: query.origURL, visitCount: query.count};
-        res.status(200).send(result);
-        
-    } else {
-        res.status(404).send();
+app.get("/url/:id", async (req, res) => {
+    try {
+        const query = await findURL("shortURL", req.params.id);
+        if (query) {
+          const result = {
+            id: query.shortURL,
+            originalURL: query.origURL,
+            visitCount: query.count,
+          };
+          res.status(200).send(result);
+        } else {
+          res.status(404).send();
+        }
+    } catch(err) {
+        next(err);
+    }
+
+});
+
+app.post("/url", async (req, res) => {
+    try {
+        const checkDupes = await findURL("origURL", req.body.url);
+        if (checkDupes) {
+            const record = {
+            id: checkDupes.shortURL,
+            originalURL: checkDupes.origURL,
+            visitCount: checkDupes.count,
+            };
+            res.status(200).send(record);
+        } else {
+            const string = generateString();
+            const result = await insertURL({
+            shortURL: string,
+            origURL: req.body.url,
+            count: 0,
+            });
+            if (result.acknowledged === true) {
+            res
+                .status(201)
+                .send({ id: string, originalURL: req.body.url, visitCount: 0 });
+            }
+        }
+    } catch(err) {
+        next(err);
     }
 });
 
-app.post('/url', async (req, res) => {
-    const checkDupes = await findOne("origURL", req.body.url);
-    if(checkDupes) {
-        const record = {id: checkDupes.shortURL, originalURL: checkDupes.origURL, visitCount: checkDupes.count};
-        res.status(200).send(record);
-    } else {
-        const string = generateString();
-        const result = await insertOne({shortURL: string, origURL: req.body.url, count: 0});
-        if(result.acknowledged === true) {
-            res.status(201).send({ "id": string, "originalURL": req.body.url, "visitCount": 0})
+app.post("/url/:id/visit", async (req, res) => {
+    try {
+        const query = await incrementURLVisitCount(req.params.id);
+        if (query.value) {
+            const result = {
+            id: query.value.shortURL,
+            originalURL: query.value.origURL,
+            visitCount: query.value.count,
+            };
+            res.status(200).send(result);
+        } else {
+            res.status(404).send();
         }
+            } catch(err) {
+            next(err);
     }
-})
+});
 
-app.post('/url/:id/visit', async (req, res) => {
-    const query = await findOneAndUpdate(req.params.id);
-    console.log(query.value);
-    if(query.value) {
-        const result = {id: query.value.shortURL, originalURL: query.value.origURL, visitCount: query.value.count};
-        res.status(200).send(result);
-        
-    } else {
-        res.status(404).send();
-    }
-})
+const errorHandler = (err, req, res, next) => {
+    console.error(err);
+    res.status(500).json({ error: 'Request failed'})
+}
 
-app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
+app.use(errorHandler)
+app.listen(port, () =>
+  console.log(`Hello world app listening on port ${port}!`)
+);
--- backend/utils/db/queries.js
+++ backend/utils/db/queries.js
@@ -1,67 +1,68 @@
-import { MongoClient, ServerApiVersion, ObjectId } from 'mongodb';
-import "dotenv/config"
+import { MongoClient, ServerApiVersion, ObjectId } from "mongodb";
+import "dotenv/config";
 
-const url = `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASSWORD}@${process.env.DBURL}?retryWrites=true&w=majority`
-const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const url = `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASSWORD}@${process.env.DBURL}?retryWrites=true&w=majority`;
+const client = new MongoClient(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 const dbName = "urlShortener";
 
 const connectDB = async () => {
-    await client.connect();
-    console.log('connecting to mongo');
+  await client.connect();
 
-    const db = client.db(dbName);
-    return db.collection("urls");
-}
+  const db = client.db(dbName);
+  return db.collection("urls");
+};
 
-export const insertOne = async (record) => {
-    let result;
-    try {
-        let collection = await connectDB();
+export const insertURL = async (record) => {
+  let result;
+  try {
+    let collection = await connectDB();
 
-        let doc = {_id: new ObjectId(), shortURL: record.shortURL, origURL: record.origURL, count: record.count };
-        result = await collection.insertOne(doc);
-
-    } catch (err) {
-        console.log(err.stack);
-    } finally {
-        await client.close();
-        return result;
-
+    let doc = {
+      _id: new ObjectId(),
+      shortURL: record.shortURL,
+      origURL: record.origURL,
+      count: record.count,
     };
-    
-}
+    result = await collection.insertOne(doc);
+  } catch (err) {
+    console.log(err.stack);
+  } finally {
+    await client.close();
+    return result;
+  }
+};
 
-export const findOne = async (key, value) => {
-    let result;
-    try {
+export const findURL = async (key, value) => {
+  let result;
+  try {
+    let collection = await connectDB();
 
-        let collection = await connectDB();
+    let query = { [key]: value };
+    result = await collection.findOne(query);
+  } catch (err) {
+    console.log(err.stack);
+  } finally {
+    await client.close();
+    return result;
+  }
+};
 
-        let query = { [key]: value }
-        result = await collection.findOne(query)
+export const incrementURLVisitCount = async (url) => {
+  let result;
+  try {
+    let collection = await connectDB();
 
-    } catch (err) {
-        console.log(err.stack);
-    } finally {
-        await client.close();
-        return result;
-
-    };
-}
-
-export const findOneAndUpdate = async (url) => {
-    let result;
-    try {
-        let collection = await connectDB();
-
-        let query = { shortURL: url };
-        let update = {$inc: {"count": 1}};
-        result = await collection.findOneAndUpdate(query, update);
-
-    } catch (err) {
-        console.log(err.stack);
-    } finally {
-        await client.close();
-        return result;
-    }
- }
+    let query = { shortURL: url };
+    let update = { $inc: { count: 1 } };
+    result = await collection.findOneAndUpdate(query, update);
+  } catch (err) {
+    console.log(err.stack);
+  } finally {
+    await client.close();
+    return result;
+  }
+};
