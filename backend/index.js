799dc8b80e564eab857a402b8ab28db092bcf813
Reuben White <white.reuben@gmail.com>
2023-03-17 12:47:19 +1300

    changes

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,43 @@
+import process from "node:process";
 import express from "express";
 import cors from "cors";
-const app = express();
-const port = 8000;
+import { MongoClient, ServerApiVersion } from "mongodb";
+
 import { generateString } from "./utils/urlBuilder.js";
 import { insertURL, findURL, incrementURLVisitCount } from "./utils/db/queries.js";
 
+//create express instance & define port to bind to
+const app = express();
+const port = 8000;
+
+//middleware setup
 app.use(express.json());
 app.use(cors());
 
-app.get("/url/:id", async (req, res) => {
+//mongoDB settings
+const url = `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASSWORD}@${process.env.DBURL}?retryWrites=true&w=majority`;
+const client = new MongoClient(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+const dbName = "urlShortener";
+//used with the exit process handler
+let exiting = false;
+
+//create and run DB connection
+const connectDB = async () => {
+  await client.connect();
+
+  const db = client.db(dbName);
+  return db.collection("urls");
+};
+
+
+//query DB for shortURL ID
+app.get("/url/:id", async (req, res, next) => {
     try {
-        const query = await findURL("shortURL", req.params.id);
+        const query = await findURL(collection, "shortURL", req.params.id);
         if (query) {
           const result = {
             id: query.shortURL,
@@ -27,9 +54,11 @@ app.get("/url/:id", async (req, res) => {
 
 });
 
-app.post("/url", async (req, res) => {
+//create new shortened URL
+app.post("/url", async (req, res, next) => {
     try {
-        const checkDupes = await findURL("origURL", req.body.url);
+        //see if it already exists
+        const checkDupes = await findURL(collection, "origURL", req.body.url);
         if (checkDupes) {
             const record = {
             id: checkDupes.shortURL,
@@ -39,7 +68,7 @@ app.post("/url", async (req, res) => {
             res.status(200).send(record);
         } else {
             const string = generateString();
-            const result = await insertURL({
+            const result = await insertURL(collection, {
             shortURL: string,
             origURL: req.body.url,
             count: 0,
@@ -55,9 +84,10 @@ app.post("/url", async (req, res) => {
     }
 });
 
-app.post("/url/:id/visit", async (req, res) => {
+//increment visited counter when URL is used
+app.post("/url/:id/visit", async (req, res, next) => {
     try {
-        const query = await incrementURLVisitCount(req.params.id);
+        const query = await incrementURLVisitCount(collection, req.params.id);
         if (query.value) {
             const result = {
             id: query.value.shortURL,
@@ -68,17 +98,53 @@ app.post("/url/:id/visit", async (req, res) => {
         } else {
             res.status(404).send();
         }
-            } catch(err) {
+        } catch(err) {
             next(err);
     }
 });
 
+//express error handler middleware
 const errorHandler = (err, req, res, next) => {
     console.error(err);
     res.status(500).json({ error: 'Request failed'})
 }
 
+//exit handler middleware
+const exitHandler = () => {
+    //check if already exiting (e.g. ctrl+c double kill behaviour)
+    if(exiting) {
+        return;
+    }
+    exiting = true;
+    
+    console.log('Closing MongoDB connection...');
+    client.close().then(() => {
+        console.log('MongoDB connection closed.');
+        process.exit();
+    }).catch((err) => {
+        console.error("Error closing MongoDB connection:", err);
+        process.exit(1);
+    })
+}
+//initialise error handler middleware
 app.use(errorHandler)
-app.listen(port, () =>
-  console.log(`Hello world app listening on port ${port}!`)
-);
+
+//process exit listeners
+process.on('exit', exitHandler);
+process.on('SIGINT', exitHandler);
+
+//connect DB & express server
+
+let collection;
+(async () => {
+    try {
+        collection = await connectDB();
+        app.listen(port, () => {
+            console.log(`Hello world app listening on port ${port}!`)
+        });
+    } catch(e) {
+        console.log(e);
+        process.exit();
+    }
+})();
+
--- backend/utils/db/queries.js
+++ backend/utils/db/queries.js
@@ -1,26 +1,10 @@
-import { MongoClient, ServerApiVersion, ObjectId } from "mongodb";
+import { ObjectId } from "mongodb";
 import "dotenv/config";
 
-const url = `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASSWORD}@${process.env.DBURL}?retryWrites=true&w=majority`;
-const client = new MongoClient(url, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
-});
-const dbName = "urlShortener";
 
-const connectDB = async () => {
-  await client.connect();
-
-  const db = client.db(dbName);
-  return db.collection("urls");
-};
-
-export const insertURL = async (record) => {
+export const insertURL = async (collection, record) => {
   let result;
   try {
-    let collection = await connectDB();
-
     let doc = {
       _id: new ObjectId(),
       shortURL: record.shortURL,
@@ -31,38 +15,32 @@ export const insertURL = async (record) => {
   } catch (err) {
     console.log(err.stack);
   } finally {
-    await client.close();
     return result;
   }
 };
 
-export const findURL = async (key, value) => {
+export const findURL = async (collection, key, value) => {
   let result;
+ 
   try {
-    let collection = await connectDB();
-
     let query = { [key]: value };
     result = await collection.findOne(query);
   } catch (err) {
     console.log(err.stack);
   } finally {
-    await client.close();
     return result;
   }
 };
 
-export const incrementURLVisitCount = async (url) => {
+export const incrementURLVisitCount = async (collection, url) => {
   let result;
   try {
-    let collection = await connectDB();
-
     let query = { shortURL: url };
     let update = { $inc: { count: 1 } };
     result = await collection.findOneAndUpdate(query, update);
   } catch (err) {
     console.log(err.stack);
   } finally {
-    await client.close();
     return result;
   }
 };
